15f05233f5c2f4efe710a42c5466e09b3ce87089
Manotosh-055 <manotoshkarmakar20512@gmail.com>
2024-02-01 00:55:44 +0530

    fix: socket error on prod

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -11,7 +11,7 @@ import io from "socket.io-client";
 import Lottie from "react-lottie";
 import animationData from "../animations/typing.json";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://chat-app-backend-indol-chi.vercel.app";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
