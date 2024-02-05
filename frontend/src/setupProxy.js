44de01e7caa2d0c63e586ab6c42c816d57fb0939
Manotosh-055 <manotoshkarmakar20512@gmail.com>
2024-02-05 20:35:17 +0530

    roll back

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
-      //target: 'https://chat-app-backend-indol-chi.vercel.app',
+      // target: 'http://localhost:5000',
+      target: 'https://chat-app-backend-indol-chi.vercel.app',
       changeOrigin: true,
     })
   );
